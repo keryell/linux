b5ea9a830c9ca9a7c167c0b74e46c04cd02e4a41
Vasant Hegde <vasant.hegde@amd.com>
2024-02-05 11:56:00 +0000

    iommu/amd: Enable Guest Translation before registering devices
    IOMMU Guest Translation (GT) feature needs to be enabled before
    invalidating guest translations (CMD_INV_IOMMU_PAGES with GN=1).
    
    Currently GT feature is enabled after setting up interrupt handler.
    So far it was fine as we were not invalidating guest page table
    before this point.
    
    Upcoming series will introduce per device GCR3 table and it will
    invalidate guest pages after configuring. Hence move GT feature
    enablement to early_enable_iommu().
    
    Signed-off-by: Vasant Hegde <vasant.hegde@amd.com>
    Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
    Link: https://lore.kernel.org/r/20240205115615.6053-3-vasant.hegde@amd.com
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- drivers/iommu/amd/init.c
+++ drivers/iommu/amd/init.c
@@ -2772,6 +2772,7 @@ static void early_enable_iommu(struct amd_iommu *iommu)
 	iommu_enable_command_buffer(iommu);
 	iommu_enable_event_buffer(iommu);
 	iommu_set_exclusion_range(iommu);
+	iommu_enable_gt(iommu);
 	iommu_enable_ga(iommu);
 	iommu_enable_xt(iommu);
 	iommu_enable_irtcachedis(iommu);
@@ -2828,6 +2829,7 @@ static void early_enable_iommus(void)
 			iommu_disable_irtcachedis(iommu);
 			iommu_enable_command_buffer(iommu);
 			iommu_enable_event_buffer(iommu);
+			iommu_enable_gt(iommu);
 			iommu_enable_ga(iommu);
 			iommu_enable_xt(iommu);
 			iommu_enable_irtcachedis(iommu);
@@ -2841,10 +2843,8 @@ static void enable_iommus_v2(void)
 {
 	struct amd_iommu *iommu;
 
-	for_each_iommu(iommu) {
+	for_each_iommu(iommu)
 		iommu_enable_ppr_log(iommu);
-		iommu_enable_gt(iommu);
-	}
 }
 
 static void enable_iommus_vapic(void)
